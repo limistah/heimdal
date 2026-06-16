68cb90faeac98ac29f7fe5dbbf7f6fd0af3f68f6
Aleem Isiaka <aisiaka@kredete.com>
2026-06-16 03:59:50 +0100

    feat(defaults): implement import functionality

--- src/defaults/mod.rs
+++ src/defaults/mod.rs
@@ -23,6 +23,11 @@ mod diff;
 #[cfg(target_os = "macos")]
 pub use diff::*;
 
+#[cfg(target_os = "macos")]
+mod import;
+#[cfg(target_os = "macos")]
+pub use import::*;
+
 /// Returns true if defaults sync is supported on this platform.
 pub fn is_supported() -> bool {
     cfg!(target_os = "macos")
