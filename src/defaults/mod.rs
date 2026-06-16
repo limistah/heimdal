a73da86e6b3bf068a7a866409feb96ca477ec1b9
Aleem Isiaka <aisiaka@kredete.com>
2026-06-16 04:01:14 +0100

    feat(defaults): implement interactive conflict resolution

--- src/defaults/mod.rs
+++ src/defaults/mod.rs
@@ -28,6 +28,11 @@ mod import;
 #[cfg(target_os = "macos")]
 pub use import::*;
 
+#[cfg(target_os = "macos")]
+mod resolve;
+#[cfg(target_os = "macos")]
+pub use resolve::*;
+
 /// Returns true if defaults sync is supported on this platform.
 pub fn is_supported() -> bool {
     cfg!(target_os = "macos")
