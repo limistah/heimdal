aa50e33916d6b840e6f7ff414e9f8821dc619ba2
Aleem Isiaka <aisiaka@kredete.com>
2026-06-16 03:46:24 +0100

    feat(defaults): implement export functionality

--- src/defaults/mod.rs
+++ src/defaults/mod.rs
@@ -12,6 +12,12 @@ mod domains;
 #[cfg(target_os = "macos")]
 pub use domains::*;
 
+#[cfg(target_os = "macos")]
+mod export;
+
+#[cfg(target_os = "macos")]
+pub use export::*;
+
 /// Returns true if defaults sync is supported on this platform.
 pub fn is_supported() -> bool {
     cfg!(target_os = "macos")
